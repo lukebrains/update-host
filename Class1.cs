76e47066c20b657662b14326baa3646f6c43aed8
Luke Brady <lbrad23105@gmail.com>
2018-09-20 16:21:15 -0400

    Starting to mess around with PowerShell for Linux and Mac.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace update_host
+{
+  public class Person {
+   public string FirstName { get; set; }
+   public string LastName { get; set; }
+
+   public Person(string first, string last) {
+     this.FirstName = first;
+     this.LastName = last;
+   }
+  }
+
+  [CmdletAttribute(VerbsCommon.New, "Person")]
+  public class NewPersonCmdlet : Cmdlet {
+    protected override void ProcessRecord() {
+      WriteObject(new Person("Trevor", "Sullivan"));
+    }
+  }
+}
