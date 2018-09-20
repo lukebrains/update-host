46627ba42f33ab1b0abdcdf0fe08fe340f9c3410
Luke Brady <lbrad23105@gmail.com>
2018-09-20 16:27:32 -0400

    Updated .net and powershell versions.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- Class1.cs
+++ Class1.cs
@@ -1,22 +1,8 @@
 ﻿using System;
-using System.Management.Automation;
 
 namespace update_host
 {
-  public class Person {
-   public string FirstName { get; set; }
-   public string LastName { get; set; }
-
-   public Person(string first, string last) {
-     this.FirstName = first;
-     this.LastName = last;
-   }
-  }
-
-  [CmdletAttribute(VerbsCommon.New, "Person")]
-  public class NewPersonCmdlet : Cmdlet {
-    protected override void ProcessRecord() {
-      WriteObject(new Person("Trevor", "Sullivan"));
+    public class Class1
+    {
     }
-  }
 }
