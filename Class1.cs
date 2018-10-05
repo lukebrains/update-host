dcd59fc9c8e2e7402a2b0881e3ecbb0489ac0b17
Luke Brady <lbrad23105@gmail.com>
2018-10-05 11:07:06 -0400

    Have a first working version of the Get-HostUpdate command on Mac.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Management.Automation;
 
-namespace update_host
+namespace UpdateHost
 {
-    public class Class1
+    [Cmdlet(VerbsCommon.Get, "HostUpdate")] //Add this PowerShell cmdlet name
+
+    public class HostUpdate : Cmdlet //Extending Class1 from Cmdlet Class
     {
+        protected override void BeginProcessing() {
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine("Updating host " + Environment.MachineName  + ".");
+        }
+
+        protected override void ProcessRecord() {
+            if(Environment.OSVersion.ToString() == "Unix 17.7.0.0") {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo();
+                info.FileName = "brew";
+                info.Arguments = "update";
+                info.UseShellExecute = true;
+                process.StartInfo = info;
+                process.Start();
+                process.WaitForExit();
+                if(process.ExitCode != 0) {
+                    Console.WriteLine("Update failed.");
+                } else {
+                    Console.WriteLine("Update complete.");
+                }
+            }
+        }
     }
 }
+
